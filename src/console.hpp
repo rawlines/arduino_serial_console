2b882dbc99de226b569cae6e820c5c8ce1913b58
Gonzalo Perez <defaperacm@gmail.com>
2022-11-23 21:22:57 +0100

    Reorganize files

--- src/console.hpp
+++ src/console.hpp
@@ -1,7 +1,6 @@
 #ifndef __CONSOLE_H
 #define __CONSOLE_H 1
 
-#define SERIAL_BAUD_RATE      9600
 #define READING_BUFF_LENGTH   8
 #define COMMAND_MAX_LENGTH    32
 #define COMMAND_PLACEHOLDER   "> "
